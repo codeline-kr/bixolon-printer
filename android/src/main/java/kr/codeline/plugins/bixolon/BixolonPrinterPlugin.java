f8c8cfc481f0ea1af3ead8aa5d7ba82f7994aadb
슷호브 <stove99@gmail.com>
2020-09-17 19:12:09 +0900

    커밋

--- android/src/main/java/kr/codeline/plugins/bixolon/BixolonPrinterPlugin.java
+++ android/src/main/java/kr/codeline/plugins/bixolon/BixolonPrinterPlugin.java
@@ -18,7 +18,7 @@ import jpos.JposException;
 public class BixolonPrinterPlugin extends Plugin {
     BixolonPrinter printer = null;
 
-    public BixolonPrinterPlugin(){
+    public BixolonPrinterPlugin() {
 
         System.out.println("BixolonPrinterPlugin 생성");
     }
@@ -41,13 +41,13 @@ public class BixolonPrinterPlugin extends Plugin {
     public void connect(PluginCall call) {
         String ip = call.getString("ip");
 
-        if( ip==null || ip.isEmpty()){
+        if (ip == null || ip.isEmpty()) {
             try {
                 Set<CharSequence> net = BXLNetwork.getNetworkPrinters(this.getActivity(), BXLNetwork.SEARCH_WIFI_ALWAYS);
 
-                if( net.size()>0 ){
+                if (net.size() > 0) {
                     ip = "" + net.iterator().next();
-                }else{
+                } else {
                     call.error("Could not find printer in your network.");
                 }
 
@@ -57,14 +57,64 @@ public class BixolonPrinterPlugin extends Plugin {
             }
         }
 
-        boolean open = printer.printerOpen(BXLConfigLoader.DEVICE_BUS_WIFI, "SRP-330II", "192.168.30.231", false);
+        boolean open = false;
 
-        if( open ) {
+        if (ip != null && !ip.isEmpty()) {
+            open = printer.printerOpen(BXLConfigLoader.DEVICE_BUS_WIFI, "SRP-330II", "192.168.30.231", false);
+        }
+
+        if (open) {
             JSObject ret = new JSObject();
             ret.put("ip", ip);
             call.success(ret);
-        }else{
+        } else {
             call.error("Could not connect to printer.");
         }
+    }
+
+    @PluginMethod
+    public void print(PluginCall call) {
+        String name = call.getString("name");
+        System.out.println("name : " + name);
+
+        printer.beginTransactionPrint();
+
+        String data = "";
+        data = "Comp: TICKET\n" +
+                "Walton, KT12 3BS\n" +
+                "Tel: 01932 901 155\n" +
+                "123-456-789\n" +
+                "VAT No. 123456789\n\n";
+        printer.printText("TICKET\n\n", BixolonPrinter.ALIGNMENT_CENTER, BixolonPrinter.ATTRIBUTE_BOLD | BixolonPrinter.ATTRIBUTE_UNDERLINE, 2);
+        printer.printText(data, BixolonPrinter.ALIGNMENT_CENTER, BixolonPrinter.ATTRIBUTE_BOLD, 1);
+        printer.printText("Sale:       " + "19-05-2017 16:19:43\n", BixolonPrinter.ALIGNMENT_LEFT, BixolonPrinter.ATTRIBUTE_BOLD, 1);
+
+        data = "Gate:       " + "Xcover kiosk\n" +
+                "Operator:   " + "Rob\n" +
+                "Order Code: " + "263036991\n";
+        printer.printText(data, BixolonPrinter.ALIGNMENT_LEFT, 0, 1);
+        printer.printText("Qty Price  Item     Total\n", BixolonPrinter.ALIGNMENT_LEFT, BixolonPrinter.ATTRIBUTE_UNDERLINE, 1);
+        printer.printText(" 1  $8.00  PARKING  $8.00\n", BixolonPrinter.ALIGNMENT_LEFT, 0, 1);
+
+        data = "Total (inc VAT):  " + "  $8.00\n" +
+                "VAT amount (20%): " + "  $1.33\n" +
+                "CARD payment:     " + "  $8.00\n" +
+                "Change due:       " + "  $0.00\n\n";
+        printer.printText(data, BixolonPrinter.ALIGNMENT_RIGHT, BixolonPrinter.ATTRIBUTE_NORMAL, 1);
+
+        data = "Thank you for your purchase!\n" +
+                "Enjoy the show!\n" +
+                "Next year visit\n" +
+                "www.bixolon.com\n" +
+                "to buy discounted tickets.\n\n\n\n";
+        printer.printText(data, BixolonPrinter.ALIGNMENT_CENTER, BixolonPrinter.ATTRIBUTE_BOLD, 1);
+
+        printer.cutPaper();
+
+        printer.endTransactionPrint();
+
+        JSObject ret = new JSObject();
+        ret.put("result", "success");
+        call.success(ret);
     }
 }
